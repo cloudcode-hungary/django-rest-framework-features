10160d2ef737e082f1efec867b0cc869858bd992
Balina Bálint <balintb@cloudcode.hu>
2019-10-04 17:19:15 +0200

    add hierarchical name to feature model

--- rest_framework_features/models.py
+++ rest_framework_features/models.py
@@ -1,21 +1,17 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from . import querysets
-
 
 class AbstractBaseFeature(models.Model):
     class Meta:
         abstract = True
         unique_together = ('parent', 'name')
-
-    objects = querysets.FeatureQuerySet.as_manager()
+        ordering = ('hierarchical_name',)
 
     created_timestamp = models.DateTimeField(auto_now_add=True)
-    modified_timestamp = models.DateTimeField(auto_now=True)
     name = models.CharField(
         verbose_name=_('name'),
-        max_length=50,
+        max_length=64,
     )
     parent = models.ForeignKey(
         verbose_name=_('parent'),
@@ -25,9 +21,21 @@ class AbstractBaseFeature(models.Model):
         on_delete=models.CASCADE,
         related_name='children',
     )
+    hierarchical_name = models.CharField(
+        verbose_name=_('name'),
+        max_length=256,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.hierarchical_name:
+            if self.parent_id:
+                self.hierarchical_name = '/'.join([self.parent.hierarchical_name, self.name])
+            else:
+                self.hierarchical_name = self.name
+        return super().save(*args, **kwargs)
 
     def __str__(self):
-        return getattr(self, 'display_name', self.name)
+        return self.hierarchical_name
 
 
 class Feature(AbstractBaseFeature):
